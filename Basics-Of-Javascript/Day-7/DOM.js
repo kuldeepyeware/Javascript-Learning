eb90e5531afa73d5365554e50780abb1c6d4036f
Kuldeep Yeware <kuldeepyeware31@gmail.com>
2022-11-04 21:47:33 +0530

    Day 7 Progress

---
+++ Basics-Of-Javascript/Day-7/DOM.js
@@ -0,0 +1,23 @@
+// getElementById 
+
+// const mainHeading = document.getElementById("main-heading");
+// console.log(mainHeading);
+
+// queryselector
+
+// const mainHeading = document.querySelector("#main-heading");
+// const header = document.querySelector(".header");
+// console.log(header);
+
+
+// Text content and innerText
+
+// const mainHeading = document.getElementById("main-heading");
+// console.log(mainHeading.textContent);
+// console.log(mainHeading.innerText);
+
+// Change style of element
+
+const mainHeading = document.querySelector("#main-heading");
+mainHeading.style.backgroundColor = "blue";
+mainHeading.style.border = "20px solid red";
